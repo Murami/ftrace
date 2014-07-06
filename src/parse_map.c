cfb5723a8a9fa5710e79cc6c922472d66e23ee61
devill_x <devill_x@epitech.eu>
2014-07-06 19:46:41 +0200

    parse_map now return a list containing t_map_entry

--- src/parse_map.c
+++ src/parse_map.c
@@ -5,7 +5,7 @@
 ** Login   <vaur@epitech.net>
 **
 ** Started on  Sun Jul  6 18:14:04 2014 vaur
-** Last update Sun Jul  6 18:58:50 2014 vaur
+** Last update Sun Jul  6 19:45:31 2014 vaur
 */
 
 /*
@@ -16,7 +16,15 @@
 #include	<stdlib.h>
 #include	<stdio.h>
 #include	<string.h>
+#include	"map_entry.h"
+#include	"parse_map.h"
 #include	"xfcts.h"
+
+/*
+** Define
+*/
+
+#define PROC_PATH	"/proc/"
 
 
 /* #include	"map_entry.h" */
@@ -25,36 +33,38 @@
 ** Functions
 */
 
-static void	new_entry(char *str_ptr_start, char str_ptr_end, char *filename)
+static t_map_entry	*new_entry(char *str_ptr_start, char *str_ptr_end, char *filename)
 {
+  t_map_entry		*new_entry;
 
+  new_entry = xmalloc(sizeof(*new_entry));
+  new_entry->ptr_start = strtol(str_ptr_start, NULL, 16);
+  new_entry->ptr_end = strtol(str_ptr_end, NULL, 16);
+  new_entry->filename = strdup(filename);
+  return (new_entry);
 }
 
-static void	read_line_map(char *line)
+static void	read_line_map(char *line, t_list *map_info)
 {
   int		i;
   char		*str_ptr_start;
   char		*str_ptr_end;
-  char		*str_offset;
   char		*filename;
 
   i = 0;
   errno = 0;
-  /* strtok(line, "- "); */
-  /* if (strtok(NULL, "- ") == NULL) */
-  /*   printf("STRTOK FAIL\n"); */
   str_ptr_start = strtok(line, "-");
   str_ptr_end = strtok(NULL, " ");
   while (i++ < 4)
     strtok(NULL, " ");
-  filename = strtok(NULL, " ");
+  filename = strtok(NULL, " \n");
   if (filename != NULL && filename[0] == '/')
     {
-      new_entry(str_ptr_start, str_ptr_end, filename);
+      list_push_back(map_info, new_entry(str_ptr_start, str_ptr_end, filename));
     }
 }
 
-static void	read_map(FILE *file_stream)
+static void	read_map(FILE *file_stream, t_list *map_info)
 {
   char		*line;
   size_t	n;
@@ -63,19 +73,37 @@ static void	read_map(FILE *file_stream)
   n = 0;
   while (getline(&line, &n, file_stream) != -1)
     {
-      read_line_map(line);
+      read_line_map(line, map_info);
       free(line);
       line = NULL;
       n = 0;
     }
 }
 
-void	parse_map(const char *map_file)
+static char	*get_map_name(int pid)
 {
-  FILE	*file_stream;
+  char		path[256];
 
+  memset(path, 0, 256);
+  sprintf(path, "%s%d/maps", PROC_PATH, pid);
+  return (strdup(path));
+}
+
+t_list		*parse_map(int pid)
+{
+  FILE		*file_stream;
+  char		*map_file;
+  t_list	*map_info;
+
+  map_file = get_map_name(pid);
+  map_info = list_new();
+  printf("map_file: %s\n", map_file);
   if ((file_stream = fopen(map_file, "r")) == NULL)
     perror("fopen");
-  read_map(file_stream);
-  fclose(file_stream);
+  else
+    {
+      read_map(file_stream, map_info);
+      fclose(file_stream);
+    }
+  return (map_info);
 }
