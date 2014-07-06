0e0122f5d5eddee6725aa4b3cc87114ae9ef3ab4
devill_x <devill_x@epitech.eu>
2014-07-06 19:46:57 +0200

    Added missing parse_map.h

---
+++ include/parse_map.h
@@ -0,0 +1,29 @@
+/*
+** parse_map.h for ftrace in /home/vaur/epitech/inprogress/B-PSU-380/AUSP_ftrace
+**
+** Made by vaur
+** Login   <vaur@epitech.net>
+**
+** Started on  Sun Jul  6 19:17:20 2014 vaur
+** Last update Sun Jul  6 19:44:00 2014 vaur
+*/
+
+#ifndef		PARSE_MAP_H
+# define	PARSE_MAP_H
+
+/*
+** Include
+*/
+
+# include	"list.h"
+
+/*
+** Prototype
+*/
+
+/*
+** parse_map.c:
+*/
+
+t_list		*parse_map(int pid);
+#endif      /* !PARSE_MAP_H_ */
